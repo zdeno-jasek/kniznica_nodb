0fca9b43e90a5179e5d148b104eb1c85d1c4a886
zdeno-jasek <zdeno.jasek@gmail.com>
2022-12-16 17:31:06 +0100

    Update DTOOsoba.java
    komentár

--- src/main/java/sk/skolenie/kniznica/application/dto/DTOOsoba.java
+++ src/main/java/sk/skolenie/kniznica/application/dto/DTOOsoba.java
@@ -1,5 +1,11 @@
 package sk.skolenie.kniznica.application.dto;
 
+/**
+ * Data Transfer Object
+ * 
+ * K triede Osoba vyrobím Data Transfer Object,
+ * ktorý slúži na posielanie dát o osobe cez REST-ové API.
+ */
 public class DTOOsoba {
 
 	public String meno;
